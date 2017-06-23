8352128c31ef6c26ecd01bc3eecfaaf04b26aecb
PresidentQS <tre335in@gmail.com>
2017-06-22 22:38:06 -0400

    Second Commit

--- home.js
+++ home.js
@@ -1,1 +1,11 @@
-var images = []
+function doStuff(){
+	console.log('this shit works');
+}
+
+var items = [];
+
+items.push("https://picturethismaths.files.wordpress.com/2016/03/fig6bigforblog.png?w=419&h=364");
+
+function printItem(){
+	console.log(items[0]);
+}
