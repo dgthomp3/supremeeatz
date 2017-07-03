e97e3af39914d2ab80156c78af0f933aaf32ea2c
PresidentQS <tre335in@gmail.com>
2017-07-03 02:27:42 -0400

    3rd Push
    Added contact and order pages
    
    Linked all pages
    
    changes to css and home.js

--- home.js
+++ home.js
@@ -2,10 +2,13 @@ function doStuff(){
 	console.log('this shit works');
 }
 
-var items = [];
+var pictures = [
+	"https://picturethismaths.files.wordpress.com/2016/03/fig6bigforblog.png?w=419&h=364",
 
-items.push("https://picturethismaths.files.wordpress.com/2016/03/fig6bigforblog.png?w=419&h=364");
+];
 
-function printItem(){
-	console.log(items[0]);
+function showPictures(){
+	for(i = 0, len = pictures.length, i++) {
+		document.write("<img src=" + pictures[i] + "width="160" height="120"/>");
+	}
 }
